ccb10083fe9325d4e88cf05b7c5257e9f8a1f172
NicoR <nico.rychlik@student.uni-siegen.de>
2018-05-18 11:40:04 +0200

    1

--- src/Gui/Gui.java
+++ src/Gui/Gui.java
@@ -5,6 +5,15 @@ public class Gui {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
 	}
 
+	/**
+	 * @param i
+	 * @param j
+	 * @return
+	 */
+	public int add(int i, int j) {
+		return i+j;
+	}
 }
