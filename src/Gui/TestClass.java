991cb6d915de8fd55b7adcd928d26c1678da6dac
Nico Rychlik <nico.rychlik@student.uni-siegen.de>
2018-05-20 20:24:19 +0200

    changed Test class to try something with Date

--- src/Gui/TestClass.java
+++ src/Gui/TestClass.java
@@ -1,6 +1,7 @@
 package Gui;
 
 import java.sql.SQLException;
+import java.util.Date;
 
 import DataAccess.SQLManager;
 
@@ -8,7 +9,11 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		try {
-			SQLManager.getInstance().insertPersonIntoDB("Nico", "Rychlik", "Oststrasse", 24, 57234, "", "","", 0);
+			Date testdate =new Date();
+			testdate.setTime(1526836286L*1000L);
+		//	SQLManager.getInstance().createPerson("Nico", "Rychlik", "Oststrasse", 24, 57234, "", "","", 0);
+			System.out.println(testdate.toString());
+			SQLManager.getInstance().deletePerson(1);
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
