cf6982a8248e8fd901ad8824f0eb9c26577070e9
Nico Rychlik <nico.rychlik@student.uni-siegen.de>
2018-05-21 12:22:44 +0200

    Bauteileverwaltung im SQL Manager

--- src/Gui/TestClass.java
+++ src/Gui/TestClass.java
@@ -9,13 +9,17 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		try {
-			Date testdate =new Date();
-			testdate.setTime(1526836286L*1000L);
+		//	Date testdate =new Date();
+		//	testdate.setTime(1526836286L*1000L);
+		//	System.out.println(testdate.toString());
 		//	SQLManager.getInstance().createPerson("Nico", "Rychlik", "Oststrasse", 24, 57234, "", "","", 0);
-			System.out.println(testdate.toString());
-			SQLManager.getInstance().deletePerson(1);
+		//	SQLManager.getInstance().deletePerson(1);
+		//	SQLManager.getInstance().addKategorie("Test");
+		//	SQLManager.getInstance().createBauteil("Testbauteil", "link", 5.33, 10, 0, 0, "Regal", 1);
+		//	SQLManager.getInstance().removeBauteil(2, 6, 8);
+			SQLManager.getInstance().addBauteil(2, 5, 8);
 		}
-		catch (SQLException e) {
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
