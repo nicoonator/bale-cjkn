44198e58606740902e31711e743a83960d613a2b
joel <joel@joel-uni-laptop>
2018-05-21 23:18:54 +0200

    added class Personenverwaltung

---
+++ src/Logic/Personenverwaltung.java
@@ -0,0 +1,27 @@
+package Logic;
+
+public class Personenverwaltung {
+
+	public void createPerson(String vorname, String nachname, String strasse, 
+			int hausnr, int PLZ, String email) {
+		
+		//TODO
+	}
+	
+	public void deletePerson(int id) {
+		
+		//TODO
+	}
+	
+	public void modifyPerson(int id, String attribute, String newData) {
+		
+		//TODO
+	}
+	
+	public boolean checkadmin(int id) {
+		
+		//TODO
+		
+		return false;
+	}
+}
