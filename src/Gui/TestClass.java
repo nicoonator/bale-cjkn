1107a67fb2f7a2959d51571ef1181999efdd6ac0
Nico Rychlik <nico.rychlik@student.uni-siegen.de>
2018-05-28 18:26:14 +0200

    modified Testclass

--- src/Gui/TestClass.java
+++ src/Gui/TestClass.java
@@ -4,6 +4,8 @@ import java.sql.SQLException;
 import java.util.Date;
 
 import DataAccess.SQLManager;
+import Logic.Finanzverwaltung;
+import Logic.Rechnung;
 
 public class TestClass {
 
@@ -16,10 +18,13 @@ public class TestClass {
 		//	SQLManager.getInstance().deletePerson(1);
 		//	SQLManager.getInstance().addKategorie("Test");
 		//	SQLManager.getInstance().createBauteil("Testbauteil", "link", 5.33, 10, 0, 0, "Regal", 1);
-			SQLManager.getInstance().removeBauteil(2, 6, 8);
+		//	SQLManager.getInstance().removeBauteil(2, 6, 8);
 		//	SQLManager.getInstance().addBauteil(2, 5, 8);
 		//	System.out.println(new Date().getTime()/1000);
 		//	System.out.println(new Date(1527061626L*1000L).toString());
+		//	SQLManager.getInstance().createAuftrag("test", "druck", 5, 10, null);
+		//	SQLManager.getInstance().createRechnung("test", "bar", 5, 1, 8, 8, 2);
+		//	Finanzverwaltung.getInstance().exportRechnung(1);
 		}
 		catch (Exception e) {
 			e.printStackTrace();
