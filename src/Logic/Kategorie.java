cb3aae840bddcf2ef8944cde5eb1f45bbc77b33a
Nico Rychlik <nico.rychlik@student.uni-siegen.de>
2018-05-31 00:07:44 +0200

    Konstruktor in Kategorie erstellt

--- src/Logic/Kategorie.java
+++ src/Logic/Kategorie.java
@@ -6,27 +6,31 @@ package Logic;
 
 public class Kategorie {
 
-	/**
-	 * @param int1
-	 * @param string
-	 */
-	public Kategorie(int ID, String name) {
-		// TODO Auto-generated constructor stub
-	}
+
 	
-	private int ID;
+	private final int ID;
 	private String name;
 	
 	
+	
+	/**
+	 * @param iD
+	 * @param name
+	 */
+	public Kategorie(int iD, String name) {
+		super();
+		ID = iD;
+		this.name = name;
+	}
+
 	public int getID() {
 		return ID;
 	}
-	public void setID(int iD) {
-		ID = iD;
-	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
