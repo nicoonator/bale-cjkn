964f2e190a504f7356016bbaf6cf27a4816472bd
Nico Rychlik <nico.rychlik@student.uni-siegen.de>
2018-05-31 00:07:14 +0200

    Toepfe und Kassen in Finanzverwaltung

--- src/Logic/Finanzverwaltung.java
+++ src/Logic/Finanzverwaltung.java
@@ -3,6 +3,7 @@ package Logic;
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.apache.pdfbox.pdmodel.*;
 import org.apache.pdfbox.pdmodel.font.*;
@@ -19,8 +20,8 @@ public class Finanzverwaltung {
 		return instance;
 	}
 	
-	public void createRechnung(String name, String bezahlart, double betrag, int auftrag_id, int auftraggeber_id, int verwalter_id, int topf_id) throws SQLException {
-		SQLManager.getInstance().createRechnung(name, bezahlart, betrag, auftrag_id, auftraggeber_id, verwalter_id, topf_id);
+	public void createRechnung(String name, String bezahlart, double betrag, int auftrag_id, int auftraggeber_id, int verwalter_id, int topf_id) throws SQLException, DatabaseException {
+		SQLManager.getInstance().createRechnung(name, bezahlart, betrag, auftrag_id, verwalter_id, topf_id);
 	}
 	
 	public void deleteRechnung(int rechnung_id) throws SQLException, DatabaseException {
@@ -95,6 +96,27 @@ public class Finanzverwaltung {
 		SQLManager.getInstance().changeRechnungStatus(id, status);
 	}
 	
-	//TODO Create Toepfe/Kassen
+	public Topf getTopfByID(int id) throws SQLException, DatabaseException {
+		return SQLManager.getInstance().getTopfByID(id);
+	}
 	
+	public List<Topf> getAllTopf() throws SQLException {
+		return SQLManager.getInstance().getAllToepfe();
+	}
+	
+	public void modifyTopf(int id, String attribut, String newData) throws SQLException {
+		SQLManager.getInstance().modifyTopf(id, attribut, newData);
+	}
+	
+	public Kasse getKasseByID(int id) throws SQLException, DatabaseException {
+		return SQLManager.getInstance().getKasseByID(id);
+	}
+	
+	public List<Kasse> getAllKasse() throws SQLException {
+		return SQLManager.getInstance().getAllKassen();
+	}
+	
+	public void modifyKasse(int id, String attribut, String newData) throws SQLException {
+		SQLManager.getInstance().modifyKasse(id, attribut, newData);
+	}
 }
