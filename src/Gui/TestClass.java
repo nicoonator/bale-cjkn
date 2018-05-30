f90f5f73646914375c2e37ac7213c0afe0b57c59
NicoR <nico.rychlik@student.uni-siegen.de>
2018-05-30 12:46:31 +0200

    test

--- src/Gui/TestClass.java
+++ src/Gui/TestClass.java
@@ -15,7 +15,7 @@ public class TestClass {
 		//	Date testdate =new Date();
 		//	testdate.setTime(1526836286L*1000L);
 		//	System.out.println(testdate.toString());
-			SQLManager.getInstance().createPerson("Nico","Rychlik","Oststrasse",24,57234,"nico.rychlik@student.uni-siegen.de","nicoonator","admin",0);
+		//	SQLManager.getInstance().createPerson("Nico","Rychlik","Oststrasse",24,57234,"nico.rychlik@student.uni-siegen.de","nicoonator","admin",0);
 		//	SQLManager.getInstance().deletePerson(1);
 		//	SQLManager.getInstance().addKategorie("Test");
 		//	SQLManager.getInstance().createBauteil("Testbauteil", "link", 5.33, 10, 0, 0, "Regal", 1);
@@ -25,7 +25,7 @@ public class TestClass {
 		//	System.out.println(new Date(1527061626L*1000L).toString());
 		//	SQLManager.getInstance().createAuftrag("test", "druck", 5, 10, null);
 		//	SQLManager.getInstance().createRechnung("test", "bar", 5, 1, 8, 8, 2);
-		//	Finanzverwaltung.getInstance().exportRechnung(1);
+			Finanzverwaltung.getInstance().exportRechnung(1);
 		//	Bauteileverwaltung.getInstance().addKategorie("Trash");
 		}
 		catch (Exception e) {
