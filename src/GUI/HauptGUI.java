822f8b12f9e51ed9d325f4067ab28861300d3ad2
Nico Rychlik <nico.rychlik@student.uni-siegen.de>
2018-05-31 21:59:54 +0200

    Mit der HauptGui rumgespielt

--- src/GUI/HauptGUI.java
+++ src/GUI/HauptGUI.java
@@ -1,6 +1,9 @@
-package Gui;
+package GUI;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -8,12 +11,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-//import javafx.scene.control.TabPane.TabClosingPolicy;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-public class HauptGUI extends Application{
+public class HauptGUI extends Application {
+	
 	
 	boolean admin;
 	
@@ -24,14 +28,18 @@ public class HauptGUI extends Application{
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Tabs");
-		Group root = new Group();
-		Scene scene = new Scene (root, 1000, 500);
+		primaryStage.setTitle("Elab-Verwaltungstool");
+		primaryStage.setMaximized(true);
+		
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(10,10,10,10));
+		grid.setVgap(8);
+		grid.setHgap(10);
+		
 		TabPane tabPane = new TabPane();
-	    BorderPane borderPane = new BorderPane();
 	    String arr[] = {"Personenverwaltung", "Auftragsverwaltung", "Finanzverwaltung", "Bauteileverwaltung"};
 	    
-	    for (int i = 0; i < 4; i++) {
+	    for (int i = 0; i < arr.length; i++) {
 	    	//if(admin)
 	    	Tab tab = new Tab(arr[i]);
 	    	HBox hbox = new HBox();
@@ -40,33 +48,38 @@ public class HauptGUI extends Application{
 	    	hbox.getChildren().add(new Label("Tab" + i));
 	      
 	    	Button btn = new Button("Bestaetigen");
+
+	    	btn.setOnAction(e -> {
+	    		System.out.println("test");
+	    	});
+	    		
 	    	hboxbtn.getChildren().add(btn);
 	    	hboxbtn.setAlignment(Pos.BOTTOM_CENTER);
 	    	hbox.setAlignment(Pos.CENTER);
 	    	
-	    	//tab.setContent laesst nur immer eine Sache zu, ueberschreibt die andere?
 	    	tab.setContent(hboxbtn);
 	    	tab.setContent(hbox);
 	    	
-	    	
-	     
-	      
-	    	tabPane.getTabs().add(tab);
-	      
+	    	tabPane.getTabs().add(tab);  
 	    }
-	    
-	   	    
-	    //tabPane.setTabClosingPolicy oder tab.setClosable(false); 
-	    
+   	    
+	    //tabPane.setTabClosingPolicy oder tab.setClosable(false);   
 	    //tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+	
+		Scene scene = new Scene (grid, 1000, 500);
 	    
-	    
-	    borderPane.prefHeightProperty().bind(scene.heightProperty());
-	    borderPane.prefWidthProperty().bind(scene.widthProperty());
+		BorderPane borderPane = new BorderPane();
+		borderPane.setTop(tabPane);
+		borderPane.setCenter(grid);
+		
+		//grid.setTop(tabPane);
+		//grid.add
 
-	    borderPane.setCenter(tabPane);
-	    root.getChildren().add(borderPane);
+	    
 	    primaryStage.setScene(scene);
 	    primaryStage.show();
 	  }
+
+	
+	
 	}
