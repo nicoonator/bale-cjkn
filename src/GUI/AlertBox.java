9dad324d962e4590707bba5dbf2d4c1c7e656255
Nico Rychlik <nico.rychlik@student.uni-siegen.de>
2018-06-01 06:01:23 +0200

    Groesse angepasst

--- src/GUI/AlertBox.java
+++ src/GUI/AlertBox.java
@@ -17,11 +17,12 @@ public class AlertBox {
         //Block events to other windows
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
-        window.setMinWidth(250);
+        window.setMinWidth(500);
+        window.setMinHeight(250);
 
         Label label = new Label();
         label.setText(message);
-        Button closeButton = new Button("Close this window");
+        Button closeButton = new Button("OK");
         closeButton.setOnAction(e -> window.close());
 
         VBox layout = new VBox(10);
