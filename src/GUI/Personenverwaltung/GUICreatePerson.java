e3a1a05fa2b6358dfd1868915d4bc8c9440795b0
Nico Rychlik <nico.rychlik@student.uni-siegen.de>
2018-06-01 18:49:08 +0200

    PErsonenverwaltung fast fertig.
    Es fehlt nur noch die Input Control fuer die E-Mail

--- src/GUI/Personenverwaltung/GUICreatePerson.java
+++ src/GUI/Personenverwaltung/GUICreatePerson.java
@@ -12,7 +12,10 @@ import java.sql.SQLException;
 
 import Exceptions.DatabaseException;
 import GUI.AlertBox;
+import Logic.Person;
 import Logic.Personenverwaltung;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.*;
 
 public class GUICreatePerson {
@@ -73,8 +76,14 @@ public class GUICreatePerson {
 		PasswordField passwortInput = new PasswordField();
 		GridPane.setConstraints(passwortInput, 1, 7);
 		
+		Label passwortconfirmLabel = new Label("Passwort bestaetigen: ");
+		GridPane.setConstraints(passwortconfirmLabel, 0, 8);
+		
+		PasswordField passwortconfirmInput = new PasswordField();
+		GridPane.setConstraints(passwortconfirmInput, 1, 8);
+		
 		Button btn = new Button("Erstellen");
-		GridPane.setConstraints(btn, 1, 8);
+		GridPane.setConstraints(btn, 1, 9);
 		
 		
 		ComboBox<String> comboBox;
@@ -85,14 +94,17 @@ public class GUICreatePerson {
                 "Lehrstuhlperson"
         );
         comboBox.setPromptText("Rolle der Person");
-        GridPane.setConstraints(comboBox, 0, 8);
+        GridPane.setConstraints(comboBox, 0, 9);
 		
 		grid.getChildren().addAll(vornameLabel, vornameInput, nachnameLabel, nachnameInput, strasseLabel, strasseInput, hausnummerLabel, hausnummerInput, 
-				PLZLabel, PLZInput, EMailLabel, EMailInput, nutzernameLabel, nutzernameInput, passwortLabel, passwortInput, btn, comboBox);
+				PLZLabel, PLZInput, EMailLabel, EMailInput, nutzernameLabel, nutzernameInput, passwortLabel, passwortInput, btn, comboBox, passwortconfirmInput, passwortconfirmLabel);
 		
 		btn.setOnMouseClicked(e -> {
+			
+			if(passwordInputValidation(passwortInput, passwortconfirmInput) && IntegerInputValidation(PLZInput) && StringInputValidation(nutzernameInput) && StringInputValidation(vornameInput) && StringInputValidation(nachnameInput) && HausNrInputValidation(hausnummerInput) && StringInputValidation(strasseInput) && TypValidation(comboBox)){
+					
 			try{
-				//TODO InputValidation
+				
 				Personenverwaltung.getInstance().createPerson(vornameInput.getText(), nachnameInput.getText(), strasseInput.getText(), Integer.parseInt(hausnummerInput.getText()), Integer.parseInt(PLZInput.getText()), EMailInput.getText(), nutzernameInput.getText(), passwortInput.getText(), comboBox.getSelectionModel().getSelectedIndex());
 				AlertBox.display("Erfolg!", "Person erzeugt!");
 				window.close();
@@ -101,11 +113,78 @@ public class GUICreatePerson {
 			catch (SQLException | DatabaseException e1) {
 				AlertBox.display("Fehler", e1.getMessage());
 			}
-		});
+			}});
 		
         Scene scene = new Scene(grid);
         window.setScene(scene);
         window.showAndWait();
+        
+        
+        
     }
+    
+	/**
+	 * @param comboBox
+	 * @return
+	 */
+	private static boolean TypValidation(ComboBox<String> comboBox) {
+		boolean result=false;
+		if (!(comboBox.getSelectionModel().isEmpty())) {
+			result=true;
+		}
+		else AlertBox.display("Fehler", "Ein Typ muss ausgewahlt werden!");
+		return result;
+	}
 
+	private static boolean HausNrInputValidation(TextField hausnummerInput) {
+		boolean result=false;
+		if (!(hausnummerInput.getText()==null || hausnummerInput.getText().trim().isEmpty())) {
+			if (hausnummerInput.getText().trim().matches("[0-9].*")) {
+				result = true;
+			}
+			else AlertBox.display("Fehler", "Hausnummer muss mit einer Zahl beginnen!");
+		}
+		else AlertBox.display("Fehler", "Kein Textfeld darf leer sein!");
+		return result;
+	}
+	
+	private static boolean StringInputValidation(TextField tf) {
+		boolean result=false;
+		if (!(tf.getText()==null || tf.getText().trim().isEmpty())) {
+			result = true;
+		}
+		else AlertBox.display("Fehler", "Kein Textfeld darf leer sein!");
+		return result;
+	}
+	
+	private static boolean IntegerInputValidation(TextField tf) {
+		boolean result=false;
+		if (!(tf.getText()==null || tf.getText().trim().isEmpty())) {
+			try {
+				Integer.parseInt(tf.getText());
+				result = true;
+			} catch (NumberFormatException e) {
+				AlertBox.display("Fehler", "PLZ darf nur Zahlen enthalten!");
+			}
+		}
+		else AlertBox.display("Fehler", "Kein Textfeld darf leer sein!");
+		return result;
+	}
+	
+	private static boolean passwordInputValidation(PasswordField passwortInput, PasswordField passwortconfirmInput) {
+	    	boolean result=false;
+	    	if(passwortInput.getText().equals(passwortconfirmInput.getText())){
+				if (!(passwortInput.getText()==null || passwortInput.getText().trim().isEmpty())) {
+					result = true;
+				}
+				else {
+					AlertBox.display("Fehler", "Das Passwort muss angegeben werden!");
+				}
+			}
+			else {
+				AlertBox.display("Fehler", "Die Passwoerter stimmen nicht ueberein!");
+			}
+	    	return result;
+	    }
+	
 }
