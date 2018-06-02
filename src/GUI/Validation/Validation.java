fb04ae2b412a0dbd956cc7fdea77366a52ee3394
Nico Rychlik <nico.rychlik@student.uni-siegen.de>
2018-06-02 12:32:30 +0200

    Ein bisschen Farbe in die Textfelder gebracht

--- src/GUI/Validation/Validation.java
+++ src/GUI/Validation/Validation.java
@@ -3,6 +3,9 @@
  */
 package GUI.Validation;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import GUI.AlertBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
@@ -13,29 +16,40 @@ import javafx.scene.control.TextField;
  *
  */
 public class Validation {
-	public static boolean mailInputValidation(TextField eMailInput) {
+	public static boolean mailInputValidation(TextField tf) {
 		boolean result=false;
-		if (!(eMailInput.getText()==null || eMailInput.getText().trim().isEmpty())) {
-			// TODO: 
-			if (eMailInput.getText().trim().matches("[0-9].*")) {
-				result = true;
+		if (!(tf.getText()==null || tf.getText().trim().isEmpty())) {
+			String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+	           Pattern p = Pattern.compile(ePattern);
+	           Matcher m = p.matcher(tf.getText());
+	           boolean match = m.matches();
+			
+	           if(match) result=true;
+	           else AlertBox.display("Fehler", "E-Mail Adresse muss in der form 'abc@xyz.pq' sein!");
 			}
-			else AlertBox.display("Fehler", "E-Mail Adresse muss in der form 'abd@xyz.pq' sein!");
-		}
+		
 		else AlertBox.display("Fehler", "Kein Textfeld darf leer sein!");
+		
+		if (!result) tf.setStyle("-fx-text-fill: red;");
+		else tf.setStyle(null);
+		
 		return result;
 	}
 	
 
-	public static boolean HausNrInputValidation(TextField hausnummerInput) {
+	public static boolean HausNrInputValidation(TextField tf) {
 		boolean result=false;
-		if (!(hausnummerInput.getText()==null || hausnummerInput.getText().trim().isEmpty())) {
-			if (hausnummerInput.getText().trim().matches("[0-9].*")) {
+		if (!(tf.getText()==null || tf.getText().trim().isEmpty())) {
+			if (tf.getText().trim().matches("[0-9].*")) {
 				result = true;
 			}
 			else AlertBox.display("Fehler", "Hausnummer muss mit einer Zahl beginnen!");
 		}
 		else AlertBox.display("Fehler", "Kein Textfeld darf leer sein!");
+
+		if (!result) tf.setStyle("-fx-text-fill: red;");
+		else tf.setStyle(null);
+		
 		return result;
 	}
 	
@@ -43,9 +57,14 @@ public class Validation {
 	public static boolean StringInputValidation(TextField tf) {
 		boolean result=false;
 		if (!(tf.getText()==null || tf.getText().trim().isEmpty())) {
-			result = true;
+			if (!(Pattern.compile( "[0-9]" ).matcher( tf.getText() ).find())) result = true;
+			else AlertBox.display("Fehler", "Regulaere Textfelder duerfen keine Zahlen enthalten!");	
 		}
 		else AlertBox.display("Fehler", "Kein Textfeld darf leer sein!");
+
+		if (!result) tf.setStyle("-fx-text-fill: red;");
+		else tf.setStyle(null);
+		
 		return result;
 	}
 	
@@ -60,22 +79,21 @@ public class Validation {
 			}
 		}
 		else AlertBox.display("Fehler", "Kein Textfeld darf leer sein!");
+
+		if (!result) tf.setStyle("-fx-text-fill: red;");
+		else tf.setStyle(null);
+		
 		return result;
 	}
 	
 	public static boolean passwordInputValidation(PasswordField passwortInput, PasswordField passwortconfirmInput) {
 	    	boolean result=false;
 	    	if(passwortInput.getText().equals(passwortconfirmInput.getText())){
-				if (!(passwortInput.getText()==null || passwortInput.getText().trim().isEmpty())) {
-					result = true;
-				}
-				else {
-					AlertBox.display("Fehler", "Das Passwort muss angegeben werden!");
-				}
+				if (!(passwortInput.getText()==null || passwortInput.getText().trim().isEmpty())) result = true;
+				else AlertBox.display("Fehler", "Das Passwort muss angegeben werden!");
 			}
-			else {
-				AlertBox.display("Fehler", "Die Passwoerter stimmen nicht ueberein!");
-			}
+			else AlertBox.display("Fehler", "Die Passwoerter stimmen nicht ueberein!");
+			
 	    	return result;
 	    }
 	
@@ -85,6 +103,7 @@ public class Validation {
 			result=true;
 		}
 		else AlertBox.display("Fehler", "Ein Typ muss ausgewahlt werden!");
+		
 		return result;
 	}
 }
