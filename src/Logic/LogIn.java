918c2d7fdb3995f027cf815040e080d4da0a9aeb
joel <joel@joel-uni-laptop>
2018-06-09 15:02:26 +0200

    Added comments to "LogIn".

--- src/Logic/LogIn.java
+++ src/Logic/LogIn.java
@@ -8,11 +8,26 @@ public class LogIn {
 
 	private static LogIn instance;
 	
+	/**
+	 * This method ensures that at most one instance of "LogIn" exists.
+	 * 
+	 * @author 
+	 * @return an instance of "LogIn"
+	 */
 	public static LogIn getInstance(){
 		if (instance==null) instance = new LogIn();
 		return instance;
 	}
 	
+	/**
+	 * This method tries to login.
+	 * 
+	 * @author 
+	 * @param username
+	 * @param password
+	 * @return true if login is successful
+	 * @throws SQLException
+	 */
 	public boolean login(String username, String password) throws SQLException {
 		return SQLManager.getInstance().login(username, password);
 	}
