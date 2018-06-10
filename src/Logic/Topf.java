0490cb29c75af54b3c5439e2f197e9e037b59374
Nico Rychlik <nico.rychlik@student.uni-siegen.de>
2018-06-10 13:06:52 +0200

    Topf hat jetzt Kasse+ Cleanup

--- src/Logic/Topf.java
+++ src/Logic/Topf.java
@@ -1,25 +1,39 @@
 package Logic;
 
+import java.sql.SQLException;
+
+import Exceptions.DatabaseException;
+
 public class Topf {
+
+	final private int TOPF_ID;
+	private String name;
+	private double soll;
+	private double ist;
+	private int kasse_ID;
+	private String kasseName;
+	private Kasse kasse;
+	
 	/**
 	 * @param tOPF_ID
 	 * @param name
 	 * @param soll
 	 * @param ist
+	 * @param kasse_ID
+	 * @param kasseName
+	 * @throws DatabaseException 
+	 * @throws SQLException 
 	 */
-	public Topf(int tOPF_ID, String name, double soll, double ist) {
+	public Topf(int tOPF_ID, String name, double soll, double ist, Kasse kasse){
 		super();
 		TOPF_ID = tOPF_ID;
 		this.name = name;
 		this.soll = soll;
 		this.ist = ist;
+		this.kasse_ID = kasse.getKASSE_ID();
+		this.kasseName =  kasse.getName();
+		this.kasse=kasse;
 	}
-	final public int TOPF_ID;
-	private String name;
-	private double soll;
-	private double ist;
-	
-	
 	public String getName() {
 		return name;
 	}
@@ -40,6 +54,42 @@ public class Topf {
 	}
 	public int getTOPF_ID() {
 		return TOPF_ID;
+	}
+	/**
+	 * @return the kasse_ID
+	 */
+	public int getKasse_ID() {
+		return kasse_ID;
+	}
+	/**
+	 * @param kasse_ID the kasse_ID to set
+	 */
+	public void setKasse_ID(int kasse_ID) {
+		this.kasse_ID = kasse_ID;
+	}
+	/**
+	 * @return the kasseName
+	 */
+	public String getKasseName() {
+		return kasseName;
+	}
+	/**
+	 * @param kasseName the kasseName to set
+	 */
+	public void setKasseName(String kasseName) {
+		this.kasseName = kasseName;
+	}
+	/**
+	 * @return the kasse
+	 */
+	public Kasse getKasse() {
+		return kasse;
+	}
+	/**
+	 * @param kasse the kasse to set
+	 */
+	public void setKasse(Kasse kasse) {
+		this.kasse = kasse;
 	}
 	
 }
