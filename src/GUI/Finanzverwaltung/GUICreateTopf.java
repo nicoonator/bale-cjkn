df5ae19546328916a5973e46a3fd6f80161b0d41
Nico Rychlik <nico.rychlik@student.uni-siegen.de>
2018-06-10 13:05:45 +0200

    Toepfe Fertig

--- src/GUI/Finanzverwaltung/GUICreateTopf.java
+++ src/GUI/Finanzverwaltung/GUICreateTopf.java
@@ -1,5 +1,84 @@
 package GUI.Finanzverwaltung;
 
-public class GUICreateTopf {
+import java.sql.SQLException;
+import GUI.AlertBox;
+import GUI.Validation.Validation;
+import Logic.Finanzverwaltung;
+import Logic.Kasse;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-}
+public class GUICreateTopf {
+	
+	 public static void display() {
+	        Stage window = new Stage();
+	        window.setTitle("Topf Erstellen");
+	        
+	        GridPane grid = new GridPane();
+			grid.setPadding(new Insets(10,10,10,10));
+			grid.setVgap(8);
+			grid.setHgap(10);
+			
+			Label btnameLabel = new Label("Name: ");
+			GridPane.setConstraints(btnameLabel, 0, 0);
+			
+			TextField btnameInput = new TextField();
+			GridPane.setConstraints(btnameInput, 1, 0);
+			
+			Label sollLabel = new Label("Soll: ");
+			GridPane.setConstraints(sollLabel, 0, 1);
+			
+			TextField sollInput = new TextField();
+			GridPane.setConstraints(sollInput, 1, 1);
+			
+			Label istLabel = new Label("Ist: ");
+			GridPane.setConstraints(istLabel, 0, 2);
+			
+			TextField istInput = new TextField();
+			GridPane.setConstraints(istInput, 1, 2);
+			
+			ComboBox<Kasse> comboBoxKassen = new ComboBox<>();
+			comboBoxKassen.setPromptText("Kasse auswaehlen");
+			comboBoxKassen.setItems(GUIKassen.getKassen());
+			GridPane.setConstraints(comboBoxKassen, 0, 3);
+			
+			Button btn = new Button("Erstellen");
+			GridPane.setConstraints(btn, 1, 3);
+			
+			Button btnClose = new Button ("Schliessen");
+			GridPane.setConstraints(btnClose, 1, 3,1,1,HPos.RIGHT,null);
+			
+			grid.getChildren().addAll(btnameLabel,btnameInput,sollLabel,sollInput,istLabel,istInput,comboBoxKassen, btn ,btnClose);
+			
+			btn.setOnMouseClicked(e -> {
+				try {
+					if(Validation.StringInputValidation(btnameInput)&&Validation.DoubleInputValidation(sollInput)&&Validation.DoubleInputValidation(istInput)&&Validation.ComboBoxValidationKassenauswahl(comboBoxKassen)) {
+						Finanzverwaltung.getInstance().createTopf(btnameInput.getText(), Double.parseDouble(sollInput.getText()), Double.parseDouble(istInput.getText()), comboBoxKassen.getSelectionModel().getSelectedItem().getKASSE_ID());
+						AlertBox.display("Topf erzeugt", "Erfolg! Topf erzeugt!");
+						window.close();
+					} 
+				} catch (SQLException e1) {
+					AlertBox.display("Fehler", e1.getMessage());
+				}
+				});
+			
+			btnClose.setOnMouseClicked(e -> {
+				window.close();
+			});
+			
+			
+			
+	        Scene scene = new Scene(grid);
+	        window.setScene(scene);
+	        window.initModality(Modality.APPLICATION_MODAL);
+	        window.showAndWait();
+	    }	
+	}
--- src/GUI/Finanzverwaltung/GUIToepfe.java
+++ src/GUI/Finanzverwaltung/GUIToepfe.java
@@ -3,13 +3,15 @@ package GUI.Finanzverwaltung;
 import java.sql.SQLException;
 import java.util.List;
 
+import Exceptions.DatabaseException;
 import GUI.AlertBox;
-import GUI.Bauteileverwaltung.GUICreateBauteil;
-import GUI.Bauteileverwaltung.GUIWarenkorb;
 import GUI.Validation.Validation;
-import Logic.Bauteil;
-import Logic.Bauteileverwaltung;
+import Logic.Finanzverwaltung;
+import Logic.Kasse;
 import Logic.Kategorie;
+import Logic.Topf;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -26,23 +28,15 @@ import javafx.scene.layout.HBox;
 
 public class GUIToepfe {
 	Tab tab;
-	TableView<Bauteil> bauteillager;
-	List<Kategorie> kategorien = this.getKategorien();
+	TableView<Topf> toepfeTable;
+	List<Topf> toepfe = this.getToepfe();
+	List<Kasse> kassen = GUIKassen.getKassen();
 	
 	public GUIToepfe(Tab tab) {
 		this.tab=tab;
 	}
-	
-	private List<Kategorie> getKategorien() {
-		try {
-			return Bauteileverwaltung.getInstance().getAllKategorie();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}
-	}
 
+	@SuppressWarnings("unchecked")
 	public void open() {
 		BorderPane bp = new BorderPane();
 		bp.setPadding(new Insets(10,10,10,10));
@@ -67,35 +61,24 @@ public class GUIToepfe {
 		
 		// Start of LEFT
 		
-		TableColumn<Bauteil, String> kategorieColumn = new TableColumn<>("Kategorie");
-		kategorieColumn.setCellValueFactory(new PropertyValueFactory<>("kategorie_name"));
-		
-		TableColumn<Bauteil, String> nameColumn = new TableColumn<>("Name");
+		TableColumn<Topf, String> nameColumn = new TableColumn<>("Name");
 		nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
 		
-		TableColumn<Bauteil, String> linkColumn = new TableColumn<>("link");
-		linkColumn.setCellValueFactory(new PropertyValueFactory<>("link"));
+		TableColumn<Topf, Double> sollColumn = new TableColumn<>("Soll");
+		sollColumn.setCellValueFactory(new PropertyValueFactory<>("soll"));
 		
-		TableColumn<Bauteil, Double> preisColumn = new TableColumn<>("Preis");
-		preisColumn.setCellValueFactory(new PropertyValueFactory<>("preis"));
+		TableColumn<Topf, Double> istColumn = new TableColumn<>("Ist");
+		istColumn.setCellValueFactory(new PropertyValueFactory<>("ist"));
 		
-		TableColumn<Bauteil, Integer> gelagertColumn = new TableColumn<>("Gelagert");
-		gelagertColumn.setCellValueFactory(new PropertyValueFactory<>("gelagert"));
+		TableColumn<Topf, Kasse> kasseColumn = new TableColumn<>("Kasse");
+		kasseColumn.setCellValueFactory(new PropertyValueFactory<>("kasse"));
 		
-		TableColumn<Bauteil, String> ortColumn = new TableColumn<>("Ort");
-		ortColumn.setCellValueFactory(new PropertyValueFactory<>("ort"));
+		toepfeTable = new TableView<>();
+		toepfeTable.setItems(this.getToepfe());
+		toepfeTable.getColumns().addAll(nameColumn, sollColumn,istColumn, kasseColumn);
 		
-		bauteillager = new TableView<>();
-		try {
-			bauteillager.setItems(GUIWarenkorb.getBauteile());
-		} catch (SQLException e2) {
-			AlertBox.display("Fehler", e2.getMessage());
-		}
-		
-		bauteillager.getColumns().addAll(kategorieColumn, nameColumn, linkColumn, preisColumn, gelagertColumn, ortColumn);
-		
-		bauteillager.setPrefWidth(350);
-		bp.setLeft(bauteillager);
+		toepfeTable.setPrefWidth(350);
+		bp.setLeft(toepfeTable);
 		
 		// End of LEFT
 		
@@ -112,57 +95,30 @@ public class GUIToepfe {
 		
 		TextField btnameInput = new TextField();
 		GridPane.setConstraints(btnameInput, 1, 0);
+		btnameInput.setDisable(true);
 		
-		Label linkLabel = new Label("Link: ");
-		GridPane.setConstraints(linkLabel, 0, 1);
+		Label sollLabel = new Label("Soll: ");
+		GridPane.setConstraints(sollLabel, 0, 1);
 		
-		TextField linkInput = new TextField();
-		GridPane.setConstraints(linkInput, 1, 1);
+		TextField sollInput = new TextField();
+		GridPane.setConstraints(sollInput, 1, 1);
+		sollInput.setDisable(true);
 		
-		Label preisLabel = new Label("Preis: ");
-		GridPane.setConstraints(preisLabel, 0, 2);
+		Label istLabel = new Label("Ist: ");
+		GridPane.setConstraints(istLabel, 0, 2);
 		
-		TextField preisInput = new TextField();
-		GridPane.setConstraints(preisInput, 1, 2);
-		preisInput.setDisable(true);
+		TextField istInput = new TextField();
+		GridPane.setConstraints(istInput, 1, 2);
+		istInput.setDisable(true);
 		
-		Label gelagertLabel = new Label("gelagert: ");
-		GridPane.setConstraints(gelagertLabel, 0, 3);
-		
-		TextField gelagertInput = new TextField();
-		GridPane.setConstraints(gelagertInput, 1, 3);
-		
-		Label geplantLabel = new Label("geplant: ");
-		GridPane.setConstraints(geplantLabel, 0, 4);
-		
-		TextField geplantInput = new TextField();
-		GridPane.setConstraints(geplantInput, 1, 4);
-		
-		Label bestelltLabel = new Label("bestellt: ");
-		GridPane.setConstraints(bestelltLabel, 0, 5);
-		
-		TextField bestelltInput = new TextField();
-		GridPane.setConstraints(bestelltInput, 1, 5);
-		
-		Label ortLabel = new Label("Ort: ");
-		GridPane.setConstraints(ortLabel, 0, 6);
-		
-		TextField ortInput = new TextField();
-		GridPane.setConstraints(ortInput, 1, 6);
-		
-		Label kategorieLabel = new Label("Kategorie");
-		GridPane.setConstraints(kategorieLabel, 0, 7);
-		
-		ComboBox<Kategorie> comboBoxKategorie = new ComboBox<>();
-		comboBoxKategorie.setPromptText("Kategorie");
+		ComboBox<Kasse> comboBoxKassen = new ComboBox<>();
+		comboBoxKassen.setPromptText("Kasse auswaehlen");
+		comboBoxKassen.setItems(GUIKassen.getKassen());
+		GridPane.setConstraints(comboBoxKassen, 0, 3);
+		comboBoxKassen.setDisable(true);
 		
 		
-		comboBoxKategorie.getItems().addAll(kategorien);
-		
-		
-		GridPane.setConstraints(comboBoxKategorie, 1, 7);
-		
-		grid.getChildren().addAll(btnameLabel,btnameInput,linkLabel,linkInput,preisLabel,preisInput ,gelagertLabel,gelagertInput,geplantLabel,geplantInput,bestelltLabel,bestelltInput, ortLabel,ortInput,kategorieLabel,comboBoxKategorie);
+		grid.getChildren().addAll(btnameLabel,btnameInput,sollLabel,sollInput,istLabel,istInput ,comboBoxKassen);
 		bp.setCenter(grid);
 		
 		// End of Center
@@ -170,11 +126,9 @@ public class GUIToepfe {
 		//Events:
 		
 		create.setOnMouseClicked(e -> {
-			GUICreateBauteil.display();
+			GUICreateTopf.display();
 			try {
-				bauteillager.setItems(GUIWarenkorb.getBauteile());
-			} catch (SQLException e1) {
-				AlertBox.display("Fehler", e1.getMessage());
+				toepfeTable.setItems(getToepfe());
 			}
 			finally {
 				modify.setDisable(true);
@@ -183,65 +137,84 @@ public class GUIToepfe {
 		});
 		
 		modify.setOnMouseClicked(e -> {
-			Bauteil tempBauteil = bauteillager.getSelectionModel().getSelectedItem();
+			Topf tempTopf = toepfeTable.getSelectionModel().getSelectedItem();
 			try {
-				if(Validation.StringInputValidation(btnameInput) && Validation.StringInputValidation(linkInput) &&Validation.IntegerInputValidation(gelagertInput) &&Validation.IntegerInputValidation(geplantInput) &&Validation.IntegerInputValidation(bestelltInput) &&Validation.StringInputValidation(ortInput)&&Validation.ComboBoxValidationKategorie(comboBoxKategorie)) {
-					Bauteileverwaltung.getInstance().modifyBauteil(tempBauteil.getID(), "name", btnameInput.getText());
-					Bauteileverwaltung.getInstance().modifyBauteil(tempBauteil.getID(), "link", linkInput.getText());
-					Bauteileverwaltung.getInstance().modifyBauteil(tempBauteil.getID(), "gelagert", gelagertInput.getText());
-					Bauteileverwaltung.getInstance().modifyBauteil(tempBauteil.getID(), "geplant", geplantInput.getText());
-					Bauteileverwaltung.getInstance().modifyBauteil(tempBauteil.getID(), "bestellt", bestelltInput.getText());
-					Bauteileverwaltung.getInstance().modifyBauteil(tempBauteil.getID(), "ort", ortInput.getText());
-					Bauteileverwaltung.getInstance().modifyBauteil(tempBauteil.getID(), "KATEGORIE_ID", String.valueOf(comboBoxKategorie.getSelectionModel().getSelectedItem().getID()));
-					bauteillager.setItems(GUIWarenkorb.getBauteile());
-				}
-			} catch (SQLException e1) {
+					if(Validation.StringInputValidation(btnameInput)&&Validation.DoubleInputValidation(sollInput)&&Validation.DoubleInputValidation(istInput)) {
+						Finanzverwaltung.getInstance().modifyTopf(tempTopf.getTOPF_ID(), "name", btnameInput.getText());
+						Finanzverwaltung.getInstance().modifyTopf(tempTopf.getTOPF_ID(), "ist", istInput.getText());
+						Finanzverwaltung.getInstance().modifyTopf(tempTopf.getTOPF_ID(), "soll", sollInput.getText());
+						Finanzverwaltung.getInstance().modifyTopf(tempTopf.getTOPF_ID(), "KASSE_ID", String.valueOf(comboBoxKassen.getSelectionModel().getSelectedItem().getKASSE_ID()));
+					}
+					toepfeTable.setItems(getToepfe());
+				} catch (SQLException e1) {
 			AlertBox.display("Fehler", e1.getMessage());
 			} finally {
-				bauteillager.getSelectionModel().select(tempBauteil);
+				toepfeTable.getSelectionModel().select(tempTopf);
 			}
-			
 		});
 		
 		delete.setOnMouseClicked(e -> {
-			Bauteil tempBauteil = bauteillager.getSelectionModel().getSelectedItem();
+			Topf tempTopf = toepfeTable.getSelectionModel().getSelectedItem();
 			try {
-				Bauteileverwaltung.getInstance().deleteBauteilByID(tempBauteil.getID());
-				bauteillager.setItems(GUIWarenkorb.getBauteile());
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+				Finanzverwaltung.getInstance().deleteTopf(tempTopf.getTOPF_ID());
+				toepfeTable.setItems(this.getToepfe());
+			} catch (SQLException | DatabaseException e1) {
+				AlertBox.display("Fehler", e1.getMessage());
 			} finally {
 				modify.setDisable(true);
 				delete.setDisable(true);
+				btnameInput.setDisable(true);
+				btnameInput.clear();
+				sollInput.setDisable(true);
+				sollInput.clear();
+				istInput.setDisable(true);
+				istInput.clear();
 			}
 			
 		});
 		
-		bauteillager.setOnMouseClicked(e -> {
-			if(!(bauteillager.getSelectionModel().isEmpty())) {
+		toepfeTable.setOnMouseClicked(e -> {
+			if(!(toepfeTable.getSelectionModel().isEmpty())) {
 				modify.setDisable(false);
 				delete.setDisable(false);
-				Bauteil tempBauteil = bauteillager.getSelectionModel().getSelectedItem();
+				Topf tempTopf = toepfeTable.getSelectionModel().getSelectedItem();
 				bp.setCenter(grid);
-				btnameInput.setText(tempBauteil.getName());
+				btnameInput.setText(tempTopf.getName());
 				btnameInput.setStyle(null);
-				linkInput.setText(tempBauteil.getLink());
-				linkInput.setStyle(null);
-				preisInput.setText(String.valueOf(tempBauteil.getPreis()));
-				preisInput.setStyle(null);
-				gelagertInput.setText(Integer.toString(tempBauteil.getGelagert()));
-				gelagertInput.setStyle(null);
-				geplantInput.setText(Integer.toString(tempBauteil.getGeplant()));
-				geplantInput.setStyle(null);
-				bestelltInput.setText(Integer.toString(tempBauteil.getBestellt()));
-				bestelltInput.setStyle(null);
-				ortInput.setText(tempBauteil.getOrt());
-				ortInput.setStyle(null);
-				//comboBoxKategorie.getSelectionModel().select(this.indexOf(tempBauteil.getKategorie_ID()));
+				btnameInput.setDisable(false);
+				sollInput.setText(String.valueOf(tempTopf.getSoll()));
+				sollInput.setStyle(null);
+				sollInput.setDisable(false);
+				istInput.setText(String.valueOf(tempTopf.getIst()));
+				istInput.setStyle(null);
+				istInput.setDisable(false);
+				comboBoxKassen.getSelectionModel().select(this.indexOf(tempTopf.getKasse_ID()));
+				comboBoxKassen.setDisable(false);
 			}
 		});
-
+		
 		tab.setContent(bp);
 	}
+
+	private ObservableList<Topf> getToepfe() {
+		ObservableList<Topf> result = FXCollections.observableArrayList();
+		try {
+			for (Topf t :  Finanzverwaltung.getInstance().getAllTopf()) {
+				result.add(t);
+				}
+		} catch (SQLException | DatabaseException e) {
+			AlertBox.display("Fehler", e.getMessage());
+		}
+		return result;
+	}
+	
+	private int indexOf(int id) {
+		int i=0;
+		for(Kasse k: kassen){
+			if(k.getKASSE_ID()==id) return i;
+			i++;
+		}
+		return -1;
+	}
+	
 }
